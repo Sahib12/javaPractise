48cdd183131ec5e62c4d33b0fec076521ef40a06
Sahib <navlanisahib1@gmail.com>
2020-11-28 19:47:16 +0530

    third commit

--- src/main/java/com/example/demo/OnlyController.java
+++ src/main/java/com/example/demo/OnlyController.java
@@ -2,12 +2,14 @@ package com.example.demo;
 
 import java.util.ArrayList;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 public class OnlyController {
 	
